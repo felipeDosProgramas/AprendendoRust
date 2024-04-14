b5fb0e689dd8bd16e7244fd9f48e489d7859c9ca
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2024-04-14 19:27:03 -0300

    criação da "classe" UsuarioLegal

--- src/usuario.rs
+++ src/usuario.rs
@@ -1,41 +1,58 @@
 pub struct UsuarioLegal {
     id: u32,
-    pub nome: String
+    pub nome: String,
+    pub e_adm: bool
 }
+fn usuario_e_adm(nome: &str) -> bool{
+    let administradores: Vec<&str> = vec!["sergio","maria","jose"];
+    let mut e_adm: bool = false;
+    for adm in administradores{
+        if adm == nome{
+            e_adm = true;
+        }
+    }
+    e_adm
+}/*
 fn criar_usuario (id: u32, nome: &str) -> UsuarioLegal {
     UsuarioLegal{
         id,
-        nome: String::from(nome)
+        nome: String::from(nome),
+        e_adm: usuario_e_adm(nome)
     }
-}
+}*/
 
 impl UsuarioLegal {
+    fn new (id: u32, nome: &str) -> UsuarioLegal {
+        UsuarioLegal{
+            id,
+            nome: String::from(nome),
+            e_adm: usuario_e_adm(nome)
+        }
+    }
     fn printa_usuario (&self) -> &UsuarioLegal {
-        println!("Hello, {}. Você é o número {}",
-            self.nome, self.id);
+        println!("Hello, {}. Você é o número {} e {}",
+            self.nome, self.id,  if self.e_adm { "felizmente é adm" } else { "infelizmente não é adm" });
         self
     }
     fn altera_nome (&self, novo_nome: &str) -> Self {
         Self {
             id: self.id,
-            nome: novo_nome.to_string()
+            nome: novo_nome.to_string(),
+            e_adm: usuario_e_adm(novo_nome)
         }
     }
     fn salvar_usuario (&self , lista_usuario: &mut Vec<UsuarioLegal>) {
         lista_usuario.push(Self{
             id: self.id,
-            nome: self.nome.clone()
+            nome: self.nome.clone(),
+            e_adm: self.e_adm
         })
     }
 
 }
 pub fn criar_todos_usuarios (mut indice: u32, nomes: Vec<&str>, mut usuarios: &mut Vec<UsuarioLegal>){
     for nome in nomes {
-        let usuario: UsuarioLegal = criar_usuario(indice, nome);
-        usuario
-            .printa_usuario()
-            .altera_nome("maria")
-            .altera_nome("sergio")
+        UsuarioLegal::new(indice, nome)
             .printa_usuario()
             .salvar_usuario(&mut usuarios);
         indice += 1;
